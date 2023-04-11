41932c0c8a8692c8db4b0432f17333c17514d015
gopinath.a.muthusamy <gopinath.a.muthusamy@accenture.com>
2023-04-11 18:47:16 +0100

    what:html report integarted

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,8 +1,7 @@
-// *** Let name it cucumber-html-report.js **
 const report = require("multiple-cucumber-html-reporter");
 report.generate({
     jsonDir: "cypress/reports/cucumber-json", // ** Path of .json file **//
-    reportPath: "cypress/reports/cucumber-html/cucumber-htmlreport.html",
+    reportPath: "cypress/reports/cucumber-htmlreport.html",
     metadata: {
         browser: {
             name: "chrome",
